fccca5e68dfc4bd33ca285523ca4e05e62e26d00
Pizzachamp <41555495+Pizzachamp@users.noreply.github.com>
2022-10-27 11:18:58 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 var xval = 0;
+var endlength = 0;
 onEvent("run", "click", function( ) {
   if (getText("run") == "run") {
     for (var i = 2; i <= getNumber("text_input1"); i++) {
@@ -6,14 +7,16 @@ onEvent("run", "click", function( ) {
       xval = xval + (100/i);
       setPosition("button"+(i), xval, getYPosition("button"+(i-1))-20, 100, 20);
       setText("label2", (xval+100)/100);
+      endlength = i;
     }
     setText("run", "reset");
   } else {
-    for (var j = 2; j <= getNumber("text_input1"); j++) {
+    for (var j = 2; j <= endlength; j++) {
       deleteElement("button"+(j));
       setText("run", "run");
     }
     xval = 0;
+    endlength = 0;
     setText("label2", "1.00");
   }
   
